a849ae5e07fcdc4fb63a80ec78806b1b98f65fdf
Mariza Buntubwimana <mbuntubw@c1r15s15.42lisboa.com>
2021-01-10 23:56:45 +0000

    j

---
+++ Rush00/ex00/rush03.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush03.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbuntubw <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/10 21:34:53 by mbuntubw          #+#    #+#             */
+/*   Updated: 2021/01/10 21:34:57 by mbuntubw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	print_horizon(int x, char left, char middle, char right)
+{
+	if (x-- >= 1)
+	{
+		ft_putchar(left);
+		while (x-- >= 2)
+			ft_putchar(middle);
+		if (x == 0)
+			ft_putchar(right);
+		ft_putchar('\n');
+	}
+}
+
+void	print_vertical(int x2, int y1, char wall)
+{
+	int i;
+
+	while (y1-- > 2)
+	{
+		ft_putchar(wall);
+		i = 2;
+		if (x2 >= 2)
+		{
+			while (x2 > i++)
+			{
+				ft_putchar(' ');
+			}
+			ft_putchar(wall);
+			ft_putchar('\n');
+		}
+		else
+		{
+			ft_putchar('\n');
+		}
+	}
+}
+
+void	rush(int x, int y)
+{
+	if (y >= 1)
+	{
+		print_horizon(x, 'A', 'B', 'C');
+		print_vertical(x, y, 'B');
+		if (y >= 2)
+			print_horizon(x, 'A', 'B', 'C');
+	}
+}
