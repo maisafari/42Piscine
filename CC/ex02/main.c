482a2c5a61c3d26e1b45af2bd3a11d1fda44c429
Mariza Buntubwimana <mbuntubw@c1r15s15.42lisboa.com>
2021-01-11 00:01:07 +0000

    m

---
+++ CC/ex02/main.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbuntubw <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/09 22:52:07 by mbuntubw          #+#    #+#             */
+/*   Updated: 2021/01/10 16:41:38 by mbuntubw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include<unistd.h>
+
+void    ft_print_reverse_alphabet(void);
+ 
+int		main(void)
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}
