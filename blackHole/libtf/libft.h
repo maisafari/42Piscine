8ee8edc6e8a43475ad63d470196cc4a062673e62
Mariza Buntubwimana <mbuntubw@c1r17s6.42lisboa.com>
2021-02-15 21:10:25 +0000

    3

--- blackHole/libtf/libft.h
+++ blackHole/libtf/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mariza <mariza@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mbuntubw <mbuntubw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 16:56:49 by mariza            #+#    #+#             */
-/*   Updated: 2021/02/11 17:59:38 by mariza           ###   ########.fr       */
+/*   Updated: 2021/02/15 21:03:39 by mbuntubw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,10 @@
 
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
+size_t	ft_strlen(char *srt);
+size_t	ft_strlcat(char *restrict dst, const char * restrict  src, size_t dstsize);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strstr(const char *haystack, const char *needle, size_t len);
 
 #endif
--- blackHole/libtf/main.c
+++ blackHole/libtf/main.c
@@ -3,20 +3,34 @@
 
 int main () 
 {
-   char str[50];
-   char *ptr;
+	char *str;
+	char *ptr;
 
-   strcpy(str,"This is string.h library function");
-   puts(str);
+	if (!(str = (char *)malloc(sizeof(char)* 50)))
+	{
+		printf("%s", "erro mem str");
+		return(-1);
+	}
+	if (!(ptr = (char *)malloc(sizeof(char)* 50)))
+	{
+		printf("%s", "erro mem ptr");
+		return(-2);
+	}
+	strcpy(str,"This is string.h library function");
+	puts(str);
 
 //	memset(str,'$',3);
  //	printf("%s\n", str);
 
-   ft_memset(str,'$',3);
-   ptr = &(str[3]);
-   ft_bzero(ptr, 4);
-
-    printf("%s\n", str);
-   
+   //ft_memset(str,'$',3);
+  // ptr = &(str[3]);
+   //ft_bzero(ptr, 4);
+  /* strcpy(ptr,"THIS IS PTR");
+   printf("%s\n", ptr);
+   printf("%lu\n",  ft_strlcat(ptr, str, 50));
+   printf("%lu\n", strlen(ptr));
+   */
+ //printf("%s\n", ft_strchr(str, 0));
+//printf("%s\n", ft_strrchr(str, 0));
    return(0);
 }
